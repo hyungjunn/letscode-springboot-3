2e864cb45758b8ad97f43fbdc4e4dcd948243f45
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-18 19:17:47 +0900

    refactor GET method - PATH: add

--- src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
+++ src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
@@ -1,15 +1,15 @@
 package letscode.boot3.controller.calculator;
 
+import letscode.boot3.dto.calculator.request.CalculatorAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // api 입구
 public class CalculatorController {
 
     @GetMapping("/add") // HTTP method, HTTP PATH
-    public int addTwoNumbers(@RequestParam int number1, @RequestParam int number2) {
-        return number1 + number2;
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.number1() + request.number2();
     }
 
 }
