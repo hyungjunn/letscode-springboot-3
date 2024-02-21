59b9bb7fe93fe65e490a4180b0c6da6c4fa0d901
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-21 10:35:46 +0900

    feat POST method PATH /multiply

--- src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
+++ src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package letscode.boot3.controller.calculator;
 
 import letscode.boot3.dto.calculator.request.CalculatorAddRequest;
+import letscode.boot3.dto.calculator.request.CalculatorMultiplyRequest;
+import letscode.boot3.dto.calculator.request.CalculatorSubRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // api 입구
@@ -13,8 +16,12 @@ public class CalculatorController {
     }
 
     @GetMapping("/sub") // HTTP method, HTTP PATH
-    public int subTwoNumbers(CalculatorAddRequest request) {
+    public int subTwoNumbers(CalculatorSubRequest request) {
         return request.number1() - request.number2();
     }
 
+    @PostMapping("/multiply")
+    public int multiply(CalculatorMultiplyRequest request) {
+        return request.number1() * request.number2();
+    }
 }
