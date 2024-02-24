8485a6181770a7e027c69ba2fb16e725de1f99c1
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-24 12:47:41 +0900

    feat dayOfTheWeek Get API

---
+++ src/main/java/letscode/boot3/controller/user/UserController.java
@@ -0,0 +1,13 @@
+package letscode.boot3.controller.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/user")
+    public void saveUser() {
+
+    }
+}
