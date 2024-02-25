ef23a06c41955d7c1545c93f4f7e0178f4e1c38f
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-25 23:37:17 +0900

    refactor main메서드에서 로직 분리

--- src/main/java/letscode/boot3/assignment5/DiceGame.java
+++ src/main/java/letscode/boot3/assignment5/DiceGame.java
@@ -3,20 +3,27 @@ package letscode.boot3.assignment5;
 import java.util.Scanner;
 
 public class DiceGame {
+    int[] resultCounts = new int[6];
+
     public static void main(String[] args) {
         System.out.print("숫자를 입력하세요 : ");
+    }
+
+    private int inputNumber() {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
+        return scanner.nextInt();
+    }
 
-        int[] rollCounts = new int[6];
-
+    private void determineRollCounts(int a) {
         for (int i = 0; i < a; i++) {
             int randomDiceFace = (int) (Math.random() * 6);
-            rollCounts[randomDiceFace]++;
+            resultCounts[randomDiceFace]++;
         }
+    }
 
-        for (int i = 0; i < rollCounts.length; i++) {
-            System.out.printf((i+1) + "번 눈금이 %d번 나왔습니다.\n", rollCounts[i + 1]);
+    private void printResult() {
+        for (int i = 0; i < resultCounts.length; i++) {
+            System.out.printf((i + 1) + "번 눈금이 %d번 나왔습니다.\n", resultCounts[i + 1]);
         }
     }
 
