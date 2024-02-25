78dc5431b01559d7f89e8ce59d252c9c2befcee1
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-26 02:15:46 +0900

    refactor POST API saveUser()

--- src/main/java/letscode/boot3/controller/user/UserController.java
+++ src/main/java/letscode/boot3/controller/user/UserController.java
@@ -3,6 +3,7 @@ package letscode.boot3.controller.user;
 import letscode.boot3.domain.user.User;
 import letscode.boot3.dto.user.request.UserCreateRequest;
 import letscode.boot3.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,19 +16,23 @@ import java.util.List;
 public class UserController {
 
     // 유저는 여러명이니 이것을 담을 List형태의 변수를 만들어줌
-    private final List<User> users = new ArrayList<>();
+    //private final List<User> users = new ArrayList<>();
+
+    // 이제 메모리말고 디스크에 연결하기 위해 jdbcTemplate를 선언
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user(name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i + 1, users.get(i)));
-        }
-        return responses;
+        return null;
     }
 }
