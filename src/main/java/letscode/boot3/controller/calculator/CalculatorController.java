8da485340ca3bdacc5a76fc6a8c857006d9b136d
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-25 21:09:18 +0900

    fix error

--- src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
+++ src/main/java/letscode/boot3/controller/calculator/CalculatorController.java
@@ -4,10 +4,8 @@ import letscode.boot3.dto.calculator.request.CalculatorRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1")
 @RestController // 입구
 public class CalculatorController {
 
