884793cb14f1887e31841ed3afec43527251e9a3
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-25 18:17:06 +0900

    feat Post API divideTwoNumber

--- src/main/java/letscode/boot3/controller/CalculatorController.java
+++ src/main/java/letscode/boot3/controller/CalculatorController.java
@@ -2,6 +2,8 @@ package letscode.boot3.controller;
 
 import letscode.boot3.dto.request.CalculatorRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 입구
@@ -11,6 +13,9 @@ public class CalculatorController {
     public int addTwoNumber(CalculatorRequest request) { // 쿼리
         return request.getNumber1() - request.getNumber2(); // 반환값
     }
+
+    @PostMapping("/divide")
+    public int divideTwoNumber(@RequestBody CalculatorRequest request) {
+        return request.getNumber1() / request.getNumber2();
+    }
 }
-
-
