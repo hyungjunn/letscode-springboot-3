aef5642704c7115661bb7b87f2ba255e2db5e8f7
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-25 17:53:54 +0900

    feat GET API addTwoNumber

---
+++ src/main/java/letscode/boot3/controller/CalculatorController.java
@@ -0,0 +1,16 @@
+package letscode.boot3.controller;
+
+import letscode.boot3.dto.request.CalculatorRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 입구
+public class CalculatorController {
+
+    @GetMapping("/minus") // HTTP method, path
+    public int addTwoNumber(CalculatorRequest request) { // 쿼리
+        return request.getNumber1() - request.getNumber2(); // 반환값
+    }
+}
+
+
