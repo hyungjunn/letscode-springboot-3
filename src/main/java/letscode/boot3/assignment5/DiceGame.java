554abf6637e89ade3325b0b4fbcdc3e084483b2a
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-02-25 23:27:52 +0900

    refactor 주사위 던지는 횟수에 관한 로직 간단하게 리팩토링

--- src/main/java/letscode/boot3/assignment5/DiceGame.java
+++ src/main/java/letscode/boot3/assignment5/DiceGame.java
@@ -11,20 +11,8 @@ public class DiceGame {
         int[] rollCounts = new int[6];
 
         for (int i = 0; i < a; i++) {
-            double b = Math.random() * 6;
-            if (b >= 0 && b < 1) {
-                r1++;
-            } else if (b >= 1 && b < 2) {
-                r2++;
-            } else if (b >= 2 && b < 3) {
-                r3++;
-            } else if (b >= 3 && b < 4) {
-                r4++;
-            } else if (b >= 4 && b < 5) {
-                r5++;
-            } else if (b >= 5 && b < 6) {
-                r6++;
-            }
+            int randomDiceFace = (int) (Math.random() * 6);
+            rollCounts[randomDiceFace]++;
         }
 
         for (int i = 0; i < rollCounts.length; i++) {
